e2eb8288b578bec9861f686bfd2789994c0ff18e
Maddie Waldie <maddiewaldie@me.com>
2020-10-22 15:29:00 -0700

    edited lab5

--- Lab5/LEDFlash/LEDFlash.ino
+++ Lab5/LEDFlash/LEDFlash.ino
@@ -6,7 +6,7 @@ const int led3Pin = 11;
 
 const int rate1 = 250;
 const int rate2 = 500;
-const int rate3 = 100;
+const int rate3 = 50;
 
 void setup() {
   pinMode(led1Pin, OUTPUT);
@@ -15,18 +15,24 @@ void setup() {
 }
 
 void loop() {
+  loop2();
+  loop3();
   digitalWrite(led1Pin, HIGH);   // turn the LED on
   delay(rate1);                       // wait for a second
   digitalWrite(led1Pin, LOW);    // turn the LED off
   delay(rate1);                       // wait for a second
+}
 
+void loop2() {
   digitalWrite(led2Pin, HIGH);   // turn the LED on
   delay(rate2);                       // wait for a second
   digitalWrite(led2Pin, LOW);    // turn the LED off
   delay(rate2);                       // wait for a second
+}
 
+void loop3() {
   digitalWrite(led3Pin, HIGH);   // turn the LED on
   delay(rate3);                       // wait for a second
   digitalWrite(led3Pin, LOW);    // turn the LED off
-  delay(rate3);                       // wait for a second
+  delay(rate3);
 }
